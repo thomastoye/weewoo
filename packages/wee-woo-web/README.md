2e334e8da448af0500d273f7cbd69d20a002eb84
Thomas Toye <thomas@toye.io>
2020-11-12 20:29:23 +0100

    Update to Angular 11

--- packages/wee-woo-web/README.md
+++ packages/wee-woo-web/README.md
@@ -1,6 +1,6 @@
 # WeeWooWeb
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.2.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.0.
 
 ## Development server
 
--- packages/wee-woo-web/src/environments/environment.ts
+++ packages/wee-woo-web/src/environments/environment.ts
@@ -4,6 +4,15 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: 'AIzaSyBlCSB4kx88-K0IrGIWncUCY_3mVDdExSk',
+    authDomain: 'weewoo-prod.firebaseapp.com',
+    databaseURL: 'https://weewoo-prod.firebaseio.com',
+    projectId: 'weewoo-prod',
+    storageBucket: 'weewoo-prod.appspot.com',
+    messagingSenderId: '950095545875',
+    appId: '1:950095545875:web:faa569390c693c5dd4e1e6',
+  },
 }
 
 /*
